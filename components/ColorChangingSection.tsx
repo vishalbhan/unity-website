6fd55aad77c7e1e00e05277c82b77bcc68c9f918
Vishal Bhan <vishalbhan312@gmail.com>
2024-01-28 20:56:35 +0530

    fixed gsap import type error

--- components/ColorChangingSection.tsx
+++ components/ColorChangingSection.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { gsap } from "gsap/dist/gsap";
+import gsap from "gsap";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 import styled from "styled-components";
 
