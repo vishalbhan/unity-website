decf9943c26451779580321e3d650f33a0e109e6
Vishal Bhan <vishalbhan312@gmail.com>
2024-02-16 01:39:30 +0530

    build fix

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const withBuilderDevTools = require("@builder.io/dev-tools/next")();
 /** @type {import('next').NextConfig} */
 const nextConfig = withBuilderDevTools({
   reactStrictMode: true,
+  "react/no-unescaped-entities": "off",
 });
 
 module.exports = nextConfig;
