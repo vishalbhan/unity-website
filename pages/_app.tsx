0458ecf259d0b366f1382deec71a717562097586
Vishal Bhan <vishalbhan312@gmail.com>
2024-02-16 01:45:07 +0530

    viewport meta tag fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,7 +14,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <main className={archivo.className}>
         <Component {...pageProps} />
