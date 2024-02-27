ba94b9f6cffe4155b4ee6943a310b956c13ec623
Vishal Bhan <vishalbhan312@gmail.com>
2024-02-27 14:33:05 +0530

    feature: rive animation component

--- components/RiveAnimation.tsx
+++ components/RiveAnimation.tsx
@@ -1,19 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useRive } from '@rive-app/react-canvas';
 
 export default function RiveAnimation({ rivFile }: { rivFile: string }) {
   const { rive, RiveComponent } = useRive({
-    src: '/rive/savings.riv',
-    stateMachines: "bumpy",
+    src: rivFile,
     autoplay: true,
   });
 
+  useEffect(() => {
+    rive?.play()
+  }, [rive])
+
   return (
     <div className='h-96'>
-      <RiveComponent 
-        onMouseEnter={() => rive && rive.play()}
-        onMouseLeave={() => rive && rive.pause()}
-      />
+      <RiveComponent />
     </div>
   )
 }
