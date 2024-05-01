f4aa73107bd5d71cdfc69e3fc9737800611e2901
Vishal Bhan <vishalbhan312@gmail.com>
2024-05-01 19:14:39 +0530

    build fix 2

--- components/nav/navItems.tsx
+++ components/nav/navItems.tsx
@@ -1,116 +1,116 @@
-import { SavingsAccount, PersonalCurrentAccount, FixedDeposit, RecurringDeposit, Lockers, PersonalLoans, PersonalInsurance, BusinessOverview, BusinessCurrentAccount, MSMELoans, SupplyChainFinancing, DigitalLending, SocialInfrastructure, RegulatoryDisclosures, AboutUnity, TreasuryServices, OurTeam, CommunicationCentre, Careers, PMCBankSchemes, Investors } from "../icons";
+// import { SavingsAccount, PersonalCurrentAccount, FixedDeposit, RecurringDeposit, Lockers, PersonalLoans, PersonalInsurance, BusinessOverview, BusinessCurrentAccount, MSMELoans, SupplyChainFinancing, DigitalLending, SocialInfrastructure, RegulatoryDisclosures, AboutUnity, TreasuryServices, OurTeam, CommunicationCentre, Careers, PMCBankSchemes, Investors } from "../icons";
 
-export const navItems = {
-  personal: {
-    save: [
-      {
-        title: 'Savings Account',
-        href: '/personal/savings-account',
-        icon: <SavingsAccount />
-      },
-      {
-        title: 'Current Account',
-        href: '/personal/current-account',
-        icon: <PersonalCurrentAccount />
-      },
-      {
-        title: 'Fixed Deposit',
-        href: '/personal/fixed-deposit',
-        icon: <FixedDeposit />
-      },
-      {
-        title: 'Recurring Deposit',
-        href: '/personal/recurring-deposit',
-        icon: <RecurringDeposit />
-      },
-      {
-        title: 'Lockers',
-        href: '/personal/lockers',
-        icon: <Lockers />
-      }
-    ],
-    borrow: [
-      {
-        title: 'Personal Loan',
-        href: '/personal/loans',
-        icon: <PersonalLoans />
-      }
-    ],
-    invest: [
-      {
-        title: 'General/Health Insurance',
-        href: '/personal/insurance',
-        icon: <PersonalInsurance />
-      },
-    ]
-  },
-  business: [
-    {
-      title: 'Current Account',
-      href: '/business/current-account',
-      icon: <BusinessCurrentAccount />
-    },
-    {
-      title: 'MSME Loans',
-      href: '/business/msme-loans',
-      icon: <MSMELoans />
-    },
-    {
-      title: 'Supply Chain Financing',
-      href: '/business/supply-chain-finance',
-      icon: <SupplyChainFinancing />
-    },
-    {
-      title: 'Digital Lending',
-      href: '/business/digital-lending',
-      icon: <DigitalLending />
-    },
-    {
-      title: 'Social Infra Finance',
-      href: '/business/social-infra-finance',
-      icon: <SocialInfrastructure />
-    }
-  ],
-  company: [
-    {
-      title: 'About Unity',
-      href: '/about-us',
-      icon: <AboutUnity />
-    },
-    {
-      title: 'Treasury Services',
-      href: '/treasury-services',
-      icon: <TreasuryServices />
-    },
-    {
-      title: 'Our Team',
-      href: '/team',
-      icon: <OurTeam />
-    },
-    {
-      title: 'Communication centre',
-      href: '/communication-centre',
-      icon: <CommunicationCentre />
-    },
-    {
-      title: 'Careers',
-      href: '/careers',
-      icon: <Careers />
-    },
-    {
-      title: 'PMC Bank Schemes',
-      href: '/pmc-bank-schemes',
-      icon: <PMCBankSchemes />
-    },
-    {
-      title: 'Investors',
-      href: '/investors',
-      icon: <Investors />
-    },
-    {
-      title: 'Regulatory disclosures',
-      href: '/regulatory-disclosures',
-      icon: <RegulatoryDisclosures />
-    },
-  ]
-}
+// export const navItems = {
+//   personal: {
+//     save: [
+//       {
+//         title: 'Savings Account',
+//         href: '/personal/savings-account',
+//         icon: <SavingsAccount />
+//       },
+//       {
+//         title: 'Current Account',
+//         href: '/personal/current-account',
+//         icon: <PersonalCurrentAccount />
+//       },
+//       {
+//         title: 'Fixed Deposit',
+//         href: '/personal/fixed-deposit',
+//         icon: <FixedDeposit />
+//       },
+//       {
+//         title: 'Recurring Deposit',
+//         href: '/personal/recurring-deposit',
+//         icon: <RecurringDeposit />
+//       },
+//       {
+//         title: 'Lockers',
+//         href: '/personal/lockers',
+//         icon: <Lockers />
+//       }
+//     ],
+//     borrow: [
+//       {
+//         title: 'Personal Loan',
+//         href: '/personal/loans',
+//         icon: <PersonalLoans />
+//       }
+//     ],
+//     invest: [
+//       {
+//         title: 'General/Health Insurance',
+//         href: '/personal/insurance',
+//         icon: <PersonalInsurance />
+//       },
+//     ]
+//   },
+//   business: [
+//     {
+//       title: 'Current Account',
+//       href: '/business/current-account',
+//       icon: <BusinessCurrentAccount />
+//     },
+//     {
+//       title: 'MSME Loans',
+//       href: '/business/msme-loans',
+//       icon: <MSMELoans />
+//     },
+//     {
+//       title: 'Supply Chain Financing',
+//       href: '/business/supply-chain-finance',
+//       icon: <SupplyChainFinancing />
+//     },
+//     {
+//       title: 'Digital Lending',
+//       href: '/business/digital-lending',
+//       icon: <DigitalLending />
+//     },
+//     {
+//       title: 'Social Infra Finance',
+//       href: '/business/social-infra-finance',
+//       icon: <SocialInfrastructure />
+//     }
+//   ],
+//   company: [
+//     {
+//       title: 'About Unity',
+//       href: '/about-us',
+//       icon: <AboutUnity />
+//     },
+//     {
+//       title: 'Treasury Services',
+//       href: '/treasury-services',
+//       icon: <TreasuryServices />
+//     },
+//     {
+//       title: 'Our Team',
+//       href: '/team',
+//       icon: <OurTeam />
+//     },
+//     {
+//       title: 'Communication centre',
+//       href: '/communication-centre',
+//       icon: <CommunicationCentre />
+//     },
+//     {
+//       title: 'Careers',
+//       href: '/careers',
+//       icon: <Careers />
+//     },
+//     {
+//       title: 'PMC Bank Schemes',
+//       href: '/pmc-bank-schemes',
+//       icon: <PMCBankSchemes />
+//     },
+//     {
+//       title: 'Investors',
+//       href: '/investors',
+//       icon: <Investors />
+//     },
+//     {
+//       title: 'Regulatory disclosures',
+//       href: '/regulatory-disclosures',
+//       icon: <RegulatoryDisclosures />
+//     },
+//   ]
+// }
